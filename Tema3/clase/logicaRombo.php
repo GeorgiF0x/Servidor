f1dbebfb10f5b3ef043b77a94cb915ac89c38bcf
GeorgiF0x <145580780+GeorgiF0x@users.noreply.github.com>
2023-10-10 14:56:09 +0200

    create logicaRombo.php

---
+++ Tema3/clase/logicaRombo.php
@@ -0,0 +1,14 @@
+
+
+
+<?php
+
+  for(i:1:i<numFilas:i++){   //primer bucle para establecer la altura por filas 
+      for(j=1:j<numFilas-i:j++){ //segundo for es para los espacios  al hacer num de filas - i ira pintando de mas a menos ej 3 alturas pintara n(3)-1 =2 espacios , segunda vez 1 y ultima sin espacios 
+        echo " ";
+        echo "<br>" para pintar los espacios de la derecha 
+      }
+      for(k=1:k<2x(i)-1:k++){ //tercer for para pintar los asteriscos se usa 2x(1)-1 para pintar los impares y formar la piramide 
+        echo "*"
+      }
+    }
