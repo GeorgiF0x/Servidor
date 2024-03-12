4ebf0a7ec16725c0cf456c865e6f3293d056fe79
georgif0x <georgifox@hotmail.com>
2024-03-12 18:01:57 +0000

    marzo . Base creareada

--- MARZO/script.sql
+++ MARZO/script.sql
@@ -1,23 +1,10 @@
 CREATE DATABASE IF NOT EXISTS TiendaMarzo;
 
-
 USE TiendaMarzo;
-
 
 CREATE TABLE IF NOT EXISTS Rol(
     Id INT AUTO_INCREMENT PRIMARY KEY,
-    NombreRol varchar(255),
-    Borrado BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE IF NOT EXISTS Usuario (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre DATE,
-    Contraseña VARCHAR(255),
-    Email VARCHAR(255),
-    FechaNacimiento DATE,
-    IdRol int,
-    FOREIGN KEY (IdRol) REFERENCES Rol(Id),
+    NombreRol VARCHAR(255),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -26,15 +13,27 @@ CREATE TABLE IF NOT EXISTS Categoria (
     Nombre VARCHAR(255),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
+
 CREATE TABLE IF NOT EXISTS Producto (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     Nombre VARCHAR(255),
     Descripcion VARCHAR(255),
-    Precio Float,
+    Precio FLOAT,
     Categoria INT,
     RutaImg VARCHAR(255),
     CantidadStock INT,
     FOREIGN KEY (Categoria) REFERENCES Categoria(Id),
+    Borrado BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS Usuario (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255 ),
+    Contraseña VARCHAR(255),
+    Email VARCHAR(255),
+    FechaNacimiento DATE,
+    IdRol INT,
+    FOREIGN KEY (IdRol) REFERENCES Rol(Id),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -64,54 +63,46 @@ CREATE TABLE IF NOT EXISTS Pedido (
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-
 CREATE TABLE IF NOT EXISTS DetallePedido (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdPedido INT,
     IdProducto INT,
-    cantidad INT,
+    Cantidad INT,
     PrecioUnidad FLOAT,
-    total INT,
+    Total INT,
     FOREIGN KEY (IdPedido) REFERENCES Pedido(Id),
     FOREIGN KEY (IdProducto) REFERENCES Producto(Id),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-
-
-
-CREATE TABLE IF NOT EXISTS Carrito(
+CREATE TABLE IF NOT EXISTS Carrito (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdUsuario INT,
     IdProducto INT,
-    FOREIGN KEY (IdUsario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
     FOREIGN KEY (IdProducto) REFERENCES Producto(Id),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-
-CREATE TABLE IF NOT EXISTS Favoritos(
+CREATE TABLE IF NOT EXISTS Favoritos (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdUsuario INT,
     IdProducto INT,
-    FOREIGN KEY (IdUsario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
     FOREIGN KEY (IdProducto) REFERENCES Producto(Id),
     Borrado BOOLEAN NOT NULL DEFAULT FALSE
 );
-
-
-
 
 INSERT INTO Rol (NombreRol) VALUES
     ('Administrador'),
     ('Moderador'),
     ('Usuario');
 
-
-INSERT INTO Usuario (Nombre, Contraseña, Email, FechaNacimiento, Perfil) VALUES
+INSERT INTO Usuario (Nombre, Contraseña, Email, FechaNacimiento, IdRol) VALUES
     ('Admingeorgi', 'adminpass', 'admin@example.com', '1990-01-01', 1),
     ('Modgeorgi', 'modpass', 'moderator@example.com', '1995-05-05', 2),
     ('georgi', 'userpass', 'user@example.com', '2000-10-10', 3);
+
 
 
 
